f83823194ebb691e9ac071647452fea35371e2e7
kristinagood <79094550+kristinagood@users.noreply.github.com>
2021-04-20 22:22:15 -0400

    Algorithm

---
+++ Algorithm Sprint 3.py
@@ -0,0 +1,11 @@
+Start program
+Set roll to 0 degrees at 40 speed for 2.5 seconds
+    Spin 90 degrees for 2 seconds
+Set roll 90 degrees at 40 speed for 2.2 seconds
+    Spin 0 degrees for 2 seconds
+Set roll 0 degrees  at 40 speed for 2.8 seconds
+    Spin 90 degrees for 2 seconds
+Set roll 90 degrees at 129 speed for 1.8 seconds
+Set roll 90 degrees at 40 speed for 1.5 seconds
+Set roll 225 degrees at 106 speed for 4 seconds
+Exit program
